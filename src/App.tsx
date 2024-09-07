a715fad9a67f9138395bbbd61d91e070b58a838c
[mark-ianz] <bustillomarkian23@gmail.com>
2024-09-07 13:48:39 +0800

    added padding on root, h1 on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,10 @@ import {
 } from "./components/ui/accordion";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <p>hello</p>
-      <Button variant={"destructive"}>hlo</Button>
-      <Accordion type="single" collapsible>
+      <h1 className="font-semibold text-lg">Accordion Test</h1>
+      <Accordion type="multiple">
         <AccordionItem value="item-1">
           <AccordionTrigger>Lorem, ipsum.</AccordionTrigger>
           <AccordionContent>
@@ -26,7 +23,25 @@ function App() {
         <AccordionItem value="item-2">
           <AccordionTrigger>Lorem, ipsum dolor.</AccordionTrigger>
           <AccordionContent>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+            placeat maxime fugiat corporis laborum doloremque nesciunt?
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-3">
+          <AccordionTrigger>Lorem ipsum dolor sit.</AccordionTrigger>
+          <AccordionContent>
+            Lorem ipsum dolor sit amet consectetur.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-4">
+          <AccordionTrigger>
+            Lorem ipsum dolor sit amet, consectetur adipisicing.
+          </AccordionTrigger>
+          <AccordionContent>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus iste
+            iure repudiandae nam nesciunt autem perferendis eum est fugit saepe?
+            Doloribus suscipit totam nihil quam explicabo fuga ullam velit
+            repellat?
           </AccordionContent>
         </AccordionItem>
       </Accordion>
