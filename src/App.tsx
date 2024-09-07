b8ae49dc300245fdbfbbe86347f90b34ea0b59db
[mark-ianz] <bustillomarkian23@gmail.com>
2024-09-07 13:39:48 +0800

    installed and tried button and accordion

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { Button } from "./components/ui/button";
+import "./index.css";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "./components/ui/accordion";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <p>hello</p>
+      <Button variant={"destructive"}>hlo</Button>
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Lorem, ipsum.</AccordionTrigger>
+          <AccordionContent>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat,
+            magni.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+          <AccordionTrigger>Lorem, ipsum dolor.</AccordionTrigger>
+          <AccordionContent>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
